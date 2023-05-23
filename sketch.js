15bbf7aa2a671eae5cb4f7403a07118c4aca68eb
unknown <galadleenard@gmail.com>
2023-05-23 20:08:49 +0800

    added arc

--- sketch.js
+++ sketch.js
@@ -1,12 +1,14 @@
 
-  function setup() {
-    createCanvas(500, 500);
-  }
-  
-  function draw() {
-    background(0);
-    rect(100, 100, 20, 30);
-  }
+
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(0);
+  rect(100, 100, 20, 30);
+  arc(100, 200, 30, 30);
+}
 
 
 
