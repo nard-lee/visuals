b27ded28c9f8bde1d6755db7b495d51b587dc308
unknown <galadleenard@gmail.com>
2023-05-23 20:01:19 +0800

    initial

---
+++ sketch.js
@@ -0,0 +1,38 @@
+
+  function setup() {
+    createCanvas(500, 500);
+  }
+  
+  function draw() {
+    background(0);
+    rect(100, 100, 20, 30);
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
